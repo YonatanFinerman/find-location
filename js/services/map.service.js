325014ce2f46ada83abfafb685387b798d4feccc
YonatanFinerman <julianmode808@gmail.com>
2022-12-21 14:05:08 +0200

    add api

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -5,6 +5,7 @@ export const mapService = {
 }
 
 
+
 // Var that is used throughout this Module (not global)
 var gMap
 
@@ -39,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY =  'AIzaSyDaRU8dfDmfYH7VAnKLLM7Y2SXli9AH33Q' 
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
