826afea7afb123f18c1bb681dbb12b5bc2507969
YonatanFinerman <julianmode808@gmail.com>
2022-12-21 18:53:06 +0200

    added name to new location

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -19,14 +19,26 @@ function getLocs() {
 }
 
 function addLocation(position) {
-    // let newLoc = {'TEMP': position.lat, position.lng}
+    // console.log('position is',position)
     let newLoc = {name: 'TEMP', lat: position.lat, lng: position.lng}
+    getLocName(position).then(res=>{
+        newLoc.name = res.compound_code
+        console.log(newLoc)
+    })
+    // let newLoc = {'TEMP': position.lat, position.lng}
     locs.push(newLoc)
 }
 
 function getLocName(pos){
+    console.log(pos.lat)
+    var name
     const API_KEY = 'AIzaSyDaRU8dfDmfYH7VAnKLLM7Y2SXli9AH33Q'
+    // latlng by name
     let urk = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${API_KEY}`
-
+    // name by laglng
+    let urlName = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${pos.lat},${pos.lng}&key=AIzaSyCWNRrGApZar-RMJ5hDCH8zRLA2TDISlPc`
+    // console.log('name of location is', posNamePrm)
+   return axios.get(urlName).then(res=>res.data.plus_code)
+//    var posNamePrm = axios.get(urlName).then(console.log)
 }
 
